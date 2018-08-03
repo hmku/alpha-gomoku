0791a135159da62d251a603aa4d5d056cab0d201
charleslien <charleslien97@gmail.com>
2018-08-03 14:58:50 -0700

    improved runtime of board.as_array

--- board.py
+++ board.py
@@ -1,4 +1,5 @@
 from bitstring import BitArray
+import numpy as np
 
 class Board:
     """
@@ -8,8 +9,8 @@ class Board:
         self.x_dim = x_dim
         self.y_dim = y_dim
         size = x_dim * y_dim
-        self.black = BitArray(int=0, length=size)
-        self.white = BitArray(int=0, length=size)
+        self.black = np.zeros((self.x_dim, self.y_dim))
+        self.white = np.zeros((self.x_dim, self.y_dim))
         self.empty = {index for index in range(size)}
         self.active_player = 0
 
@@ -32,11 +33,10 @@ class Board:
             x, y = self.get_coords(index)
         else:
             x, y = move
-            index = self.get_index(x, y)
 
         if not self.valid_coord(x, y):
             return False
-        return not self.black[index] and not self.white[index]
+        return not self.black[x, y] and not self.white[x, y]
 
     def valid_moves(self):
         '''
@@ -68,8 +68,7 @@ class Board:
                 cy += dy
                 if not self.valid_coord(cx, cy):
                     break
-                index = self.get_index(cx, cy)
-                if color[index]:
+                if color[cx, cy]:
                     in_dir += 1
                 else:
                     break
@@ -79,8 +78,7 @@ class Board:
                 cy -= dy
                 if not self.valid_coord(cx, cy):
                     break
-                index = self.get_index(cx, cy)
-                if color[index]:
+                if color[cx, cy]:
                     in_dir += 1
                 else:
                     break
@@ -89,31 +87,18 @@ class Board:
         return False
 
     def as_array(self):
-        b_array = []
-        w_array = []
-        index = 0
-        for x in range(self.x_dim):
-            b_row = []
-            w_row = []
-            for y in range(self.y_dim):
-                b_row.append(1 if self.black[index] else 0)
-                w_row.append(1 if self.white[index] else 0)
-                index += 1
-            b_array.append(b_row)
-            w_array.append(w_row)
-
         if self.active_player == 0:
-            return [b_array, w_array]
+            return [self.black, self.white]
         else:
-            return [w_array, b_array]
+            return [self.white, self.black]
 
     def copy(self):
         '''
         returns a copy of the board
         '''
         copy = Board(x_dim = self.x_dim, y_dim = self.y_dim)
-        copy.white = self.white[:]
-        copy.black = self.black[:]
+        copy.white = self.white.copy()
+        copy.black = self.black.copy()
         copy.empty = set(self.empty)
         copy.active_player = self.active_player
         return copy
@@ -128,6 +113,7 @@ class Board:
 
         if isinstance(move, int):
             index = move
+            x, y = self.get_coords(index)
         else:
             x, y = move
             index = self.get_index(x, y)
@@ -135,29 +121,28 @@ class Board:
         win = self.is_winning_move(move)
 
         if self.active_player == 0:
-            self.black.set(1, index)
+            self.black[x, y] = 1
         if self.active_player == 1:
-            self.white.set(1, index)
+            self.white[x, y] = 1
         self.empty.remove(index)
         self.active_player = 1 - self.active_player
 
+        self.updated = False
         return self, win
 
     def __str__(self):
-        index = 0
         rows = []
         for x in range(self.x_dim):
             row = []
             for y in range(self.y_dim):
-                if self.black[index]:
+                if self.black[x, y]:
                     row.append('X')
-                elif self.white[index]:
+                elif self.white[x, y]:
                     row.append('O')
                 else:
                     row.append('.')
-                index += 1
             rows.append(' '.join(row))
         return '\n'.join(rows)
 
     def __repr__(self):
-        return str(self.black.bin) + str(self.white.bin)
+        return str(self.black) + str(self.white)

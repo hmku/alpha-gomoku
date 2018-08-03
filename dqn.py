0476355fa84d3b03f9d3609f133e74e8e7329ac4
Felix Wang <ploxxer31415@gmail.com>
2018-08-02 18:35:40 -0600

    implemented select_action

--- dqn.py
+++ dqn.py
@@ -1,3 +1,5 @@
+from itertools import count
+import math
 import random
 import torch
 import torch.nn as nn
@@ -6,41 +8,53 @@ import torch.nn.functional as F
 
 from board import Board
 from gomoku_net import Net
+from replay_memory import Transition, ReplayMemory
 
+BATCH_SIZE = 128
+GAMMA = 0.999
+EPS_START = 0.9
+EPS_END = 0.05
+EPS_DECAY = 200
+TARGET_UPDATE = 10
 
-class Transition():
+policy_net = Net()
+target_net = Net()
+target_net.load_state_dict(policy_net.state_dict())
+target_net.eval()
 
-    def __init__(self, state, action, next_state, reward):
-        self.state = state
-        self.action = action
-        self.next_state = next_state
-        self.reward = reward
+optimizer = optim.RMSprop(policy_net.parameters())
+memory = ReplayMemory(100)
 
+steps_done = 0
 
-class ReplayMemory(object):
+def select_action(state):
+    global steps_done
+    sample = random.random()
+    eps_threshold = EPS_END + (EPS_START - EPS_END) * \
+        math.exp(-1. * steps_done / EPS_DECAY)
+    steps_done += 1
 
-    def __init__(self, capacity):
-        self.capacity = capacity
-        self.memory = []
-        self.position = 0
+    if sample > eps_threshold:
+        # compute q-values
+        state = torch.tensor(state.as_array()).unsqueeze(0)
+        state = state.type(torch.FloatTensor)
+        with torch.no_grad():
+            q_values = net(state)
 
-    def push(self, transition):
-        """Saves a transition."""
-        if len(self.memory) < self.capacity:
-            self.memory.append(None)
-        self.memory[self.position] = transition
-        self.position = (self.position + 1) % self.capacity
+        # choose top (valid) move
+        valid_moves = state.valid_moves()
+        top_moves = torch.topk(q_values, 361)
+        top_moves = top_moves[1].squeeze(0)
+        for i in range(361):
+            top_move = top_moves[i].item()
+            if top_move in valid_moves:
+                break
+        return top_move
 
-    def sample(self, batch_size):
-        return random.sample(self.memory, batch_size)
-
-    def __len__(self):
-        return len(self.memory)
-
+    else:
+        return torch.tensor([[random.randrange(361)]], dtype=torch.long)
 
 # TODO: util function for board transformation
-
-
 
 
 
